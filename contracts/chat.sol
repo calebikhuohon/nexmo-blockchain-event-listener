7be5ddf5aa09b492fcb6fd06f7e880208b45f9d9
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-03-09 16:49:42 +0100

    refactor: clean up smart contract

--- contracts/chat.sol
+++ contracts/chat.sol
@@ -1,28 +1,21 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract TextMessage {
-    uint256 cost;
-    bool public enabled;
+    // define event
 
-    // Stores msg index + sender
-    
-    // mapping(uint256 => address) internal sendersPerIndex;
-    // Stores msg index + msg content
-    // mapping(uint256 => string) internal contentsPerIndex;
-    // Stores all the msg indexes
-    // uint256[] internal msgIndices;
+    event NewText(
+        address indexed sender,
+        address indexed receiver,
+        string content
+    );
 
-    event NewText(address sender, string content);
-
-    function sendMessage(address _sender, string memory _content) public {
-        // Calculate the next message index
-        // uint256 msgIndex = msgIndices.length;
-
-        // Store the messages
-        // sendersPerIndex[msgIndex] = _sender;
-        // contentsPerIndex[msgIndex] = _content;
-        // msgIndices.push(msgIndex);
-        emit NewText(_sender, _content);
+    function sendMessage(
+        address _sender,
+        address _receiver,
+        string memory _content
+    ) public {
+        // emit event
+        emit NewText(_sender, _receiver, _content);
     }
 
 }
